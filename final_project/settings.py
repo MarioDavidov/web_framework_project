6cba4e873d014f66029a6117393c1ab9e5663f61
Mario Davido <75420164+MarioDavidov@users.noreply.github.com>
2022-02-09 13:09:24 +0200

    'staticfiles'

--- final_project/settings.py
+++ final_project/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     join(BASE_DIR, 'static'),
 ]
